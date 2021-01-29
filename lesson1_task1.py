17e418633b071f36bf6d5b23a3ba27b0f71c1426
Оксана Цуверкалова <74481811+oksanatsuverkalova@users.noreply.github.com>
2021-01-29 21:28:16 +0300

    lesson1_task1

---
+++ lesson1_task1.py
@@ -0,0 +1,16 @@
+line_lesson1 = 'First lesson'
+print(line_lesson1)
+
+number_lesson1 = 8
+print(number_lesson1)
+
+f_number_lesson1 = 5.78
+print(f_number_lesson1)
+
+user = input('Введите логин: ')
+password = input('Введите пароль: ')
+print(user, password)
+
+user_number = input('Введите число от 1 до 100: ')
+print(user_number)
+
